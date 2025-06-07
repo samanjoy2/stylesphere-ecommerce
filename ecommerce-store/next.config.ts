b6223c1005115fcd0616cfa36b061546ef3f3642
unknown <samanjoy2@gmail.com>
2025-06-07 09:18:17 +0800

    dsdaaaa

--- ecommerce-store/next.config.ts
+++ ecommerce-store/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'stylesphere-bucket.s3.us-east-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   /* config options here */
 };
 
